eb80b905d561528b26974f837b9c748348a659a7
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2021-04-23 20:40:25 -0500

    day_09 class practice

---
+++ day_09/index.js
@@ -0,0 +1,38 @@
+console.log(2+2);
+console.log('I like JavaScript');
+
+var myName;
+var age = 20;
+var rollNumber,address;
+var y=0;
+const pi=3.14;
+
+myName= "Rahwa";
+var lastName = 'Netserab';
+var address = "421 'Sargent' avenue";
+var finalString = `My Name: ${myName} ${lastName}`;
+
+
+
+var isWeekDay = false;
+var myTotal = null;
+
+console.log(typeof finalString);
+console.log(typeof isWeekDay);
+console.log(myTotal);
+console.log(finalString);
+console.log(address);
+console.log("My Name: " + myName + " " + lastName + " " + "Age " + age);
+
+
+function add()
+{
+    let x = 4;
+    let y = 5;
+    console.log(x);
+    console.log(y);
+}
+
+add();
+//console.log(x); can not use this becuase it is not defined/declared
+console.log(y);
