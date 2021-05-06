f42657db8282a41cea19821702f0e0184454266c
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2021-05-05 21:23:14 -0500

    day 14 class added

---
+++ day_14/ex_1.js
@@ -0,0 +1,28 @@
+//forEach loop
+var pets = ['Bobby','Sam','Spot'];
+
+pets.forEach(function(pet) {
+    console.log(pet);
+});
+
+
+// it can be done like this too either the upper one of this
+    // var pets = ['Bobby','Sam','Spot'];
+
+    // pets.forEach(function(pet) {
+    //     console.log(pet);
+    //     pets[index] = pet.toUpperCase();
+    // });
+    //asynchronix programming - make a call and wait until it executes
+    //     console.log(pets);
+
+let words = ['apple','grape','kiwi','banana'];
+words.forEach(function(word){
+    console.log(word);
+    if(word === 'grape'){
+        words.shift();
+    }
+});
+//apple
+//grape
+//banana
