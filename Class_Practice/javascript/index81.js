96f8df2e99d04b92b444456317b30747461c517a
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2021-05-17 16:43:20 -0500

    practice brwoser API added

---
+++ Class_Practice/javascript/index81.js
@@ -0,0 +1,38 @@
+/**
+ * Create a new index81.js file
+Copy and paste the code from exercise 71
+ * /**
+ */ 
+
+// var i = 1;
+// var results;
+
+// while(i<11){
+//     results = 9*1;
+//     console.log(`9 times ${i} equals ${results}`);
+//     i++;
+// }
+
+var i = 1;
+var results;
+do{
+    
+    results = 9*1;
+    console.log(`9 times ${i} equals ${results}`);
+    i++;
+}
+while(i < 11){
+
+}
+/**
+ * 9 times 1 equals 9
+9 times 2 equals 9
+9 times 3 equals 9
+9 times 4 equals 9
+9 times 5 equals 9
+9 times 6 equals 9
+9 times 7 equals 9
+9 times 8 equals 9
+9 times 9 equals 9
+9 times 10 equals 9
+ */
