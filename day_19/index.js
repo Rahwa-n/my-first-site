d8d2d781a40935c201bd387d2051a27199aad0e4
rahwa3netsereab@gmail.com <rahwa3netsereab@gmail.com>
2021-05-21 21:02:47 -0500

    class 21 lesson sample project added

--- day_19/index.js
+++ day_19/index.js
@@ -9,18 +9,17 @@
 
 const express = require('express');
 const app = express();
-
 app.set('view engine', 'pug');
-
 app.get('/', function(req, res){
     res.render('index', {mytitle: 'Hey', message:'I like to code'});
 });
-
+app.get('/firstpage', function(req, res){
+    res.render('myfirstpage', {mytitle: 'Hey'});
+});
 app.listen(3002,function()
 {
     console.log('I am listening ');
 });
-
 
 
 
